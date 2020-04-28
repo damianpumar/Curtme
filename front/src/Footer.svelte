4d369251c20fbd14417543df77bc995781dec8ed
Damián Pumar <damianpumar@gmail.com>
2020-04-28 20:17:45 +0200

    Improve footer

--- front/src/Footer.svelte
+++ front/src/Footer.svelte
@@ -1,41 +1,17 @@
 <footer id="footer">
   <ul class="icons">
     <li>
-      <a href="#" class="icon brands fa-twitter">
-        <span class="label">Twitter</span>
-      </a>
-    </li>
-    <li>
-      <a href="#" class="icon brands fa-facebook-f">
-        <span class="label">Facebook</span>
-      </a>
-    </li>
-    <li>
-      <a href="#" class="icon brands fa-instagram">
-        <span class="label">Instagram</span>
-      </a>
-    </li>
-    <li>
-      <a href="#" class="icon brands fa-github">
+      <a
+        href="https://github.com/damianpumar/Curtme"
+        class="icon brands fa-github">
         <span class="label">Github</span>
-      </a>
-    </li>
-    <li>
-      <a href="#" class="icon brands fa-dribbble">
-        <span class="label">Dribbble</span>
-      </a>
-    </li>
-    <li>
-      <a href="#" class="icon brands fa-google-plus">
-        <span class="label">Google+</span>
       </a>
     </li>
   </ul>
   <ul class="copyright">
-    <li>&copy; Untitled. All rights reserved.</li>
+    <li>&copy; Curtme.org</li>
     <li>
-      Design:
-      <a href="http://html5up.net">HTML5 UP</a>
+      <a href="https://damianpumar.com" target="_blan" k>Damián Pumar</a>
     </li>
   </ul>
 </footer>
