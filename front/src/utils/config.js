34bea12d5a837c2c6045bc371dfb6e3b8cdac839
Damián Pumar <damianpumar@gmail.com>
2020-05-05 16:44:04 +0200

    Added image for auth0

--- front/src/utils/config.js
+++ front/src/utils/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://localhost:5001/";
+export const BASE_URL = "https://curtme.org/";
 export const VISIT_LINK = (shortURL) => `${BASE_URL}${shortURL}`;
 export const GET_LINKS_BY_IDS = `${BASE_URL}links-by-id`;
 export const GET_USER_LINKS = `${BASE_URL}links`;
