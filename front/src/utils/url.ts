8f2ec0ded4568edde3a134ea113515d5eb181c57
Damián Pumar <damianpumar@gmail.com>
2022-11-23 14:29:39 +0100

    🐛 Fix url

--- front/src/utils/url.ts
+++ front/src/utils/url.ts
@@ -1,14 +1,11 @@
 export const validURL = (url: string) => {
-  var pattern = new RegExp(
-    "^(https?:\\/\\/)?" + // protocol
-      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-      "(\\#[-a-z\\d_]*)?$",
-    "i"
-  );
-  return !!pattern.test(url);
+  try {
+    new URL(url);
+
+    return true;
+  } catch (_) {
+    return false;
+  }
 };
 
 export const getSourceURLFromParameter = () => {
